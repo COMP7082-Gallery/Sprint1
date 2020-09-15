0115f4fad4e08b983da290ab33d3e5ec3653f4d5
unknown <j.soukchamroeun@gmail.com>
2020-09-15 05:30:31 -0700

    09-15 // Gallery Layout

--- app/src/main/java/com/example/sprintone/MainActivity.java
+++ app/src/main/java/com/example/sprintone/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.sprintone;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
