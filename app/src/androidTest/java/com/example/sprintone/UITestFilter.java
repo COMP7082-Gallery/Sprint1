c079666fbc96acd8e4c692e7cdca5db92733a882
unknown <j.soukchamroeun@gmail.com>
2020-09-21 10:18:36 -0700

    09-21 UI Tests for Search Functionality

--- app/src/androidTest/java/com/example/sprintone/UITestFilter.java
+++ app/src/androidTest/java/com/example/sprintone/UITestFilter.java
@@ -9,15 +9,32 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class UITestFilter {
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule =
+            new ActivityTestRule<>(MainActivity.class);
 
+    @Test
+    public void listGoesOverTheFold() {
+        onView(withId(R.id.filter_image_btn)).perform(click());
+        onView(withId(R.id.filter_start_date)).perform(typeText(""), closeSoftKeyboard());
+        onView(withId(R.id.filter_end_date)).perform(typeText(""), closeSoftKeyboard());
+        onView(withId(R.id.filter_keyword)).perform(typeText("caption"), closeSoftKeyboard());
+        onView(withId(R.id.search_filter_btn)).perform(click());
+        onView(withId(R.id.captionText)).check(matches(withText("caption")));
+        onView(withId(R.id.next_btn)).perform(click());
+        onView(withId(R.id.captionText)).check(matches(withText("caption")));
+        onView(withId(R.id.prev_btn)).perform(click());
+        onView(withId(R.id.prev_btn)).perform(click());
+        onView(withId(R.id.captionText)).check(matches(withText("caption")));
+    }
 }
