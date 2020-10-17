3e9aafb65e67f2bf0e93bd03a36a8fb6491ca7ee
Lydia-l-YU <ylznz1227@gmail.com>
2020-10-17 00:18:00 -0700

    Added lambda expression with Stream API in MainActivity. Added input filter class to limit user input for latitude and longitude based search. Show a toast when the end date picked by user is earlier than the start date.
    Signed-off-by: Lydia-l-YU <ylznz1227@gmail.com>

--- app/src/main/java/com/example/sprintone/filter_gallery/FilterGalleryActivity.java
+++ app/src/main/java/com/example/sprintone/filter_gallery/FilterGalleryActivity.java
@@ -5,20 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.TextWatcher;
 import android.view.View;
-import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.sprintone.R;
+import com.example.sprintone.numberfilter.InputFilterMinMax;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 
 public class FilterGalleryActivity extends AppCompatActivity {
 
     private EditText startDate;
     private EditText endDate;
+    private EditText tlLat;
+    private EditText tlLon;
+    private EditText brLat;
+    private EditText brLon;
+    private Date sDate;
+    private Date eDate;
     private int dateLabel = 0;
     private final Calendar myCalendar = Calendar.getInstance();
 
@@ -29,50 +40,58 @@ public class FilterGalleryActivity extends AppCompatActivity {
 
         startDate = findViewById(R.id.filter_start_date);
         endDate = findViewById(R.id.filter_end_date);
+        tlLat = findViewById(R.id.tlLat);
+        tlLon = findViewById(R.id.tlLon);
+        brLat = findViewById(R.id.brLat);
+        brLon = findViewById(R.id.brLon);
 
-        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
-
-            @Override
-            public void onDateSet(DatePicker view, int year, int monthOfYear,
-                                  int dayOfMonth) {
-                // TODO Auto-generated method stub
-                myCalendar.set(Calendar.YEAR, year);
-                myCalendar.set(Calendar.MONTH, monthOfYear);
-                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-                updateLabel(dateLabel);
-            }
+        final DatePickerDialog.OnDateSetListener date = (view, year, monthOfYear, dayOfMonth) -> {
+            // TODO Auto-generated method stub
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel(dateLabel);
         };
 
-        startDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                new DatePickerDialog(FilterGalleryActivity.this, date, myCalendar
-                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
-                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
-                dateLabel = 1;
-            }
+        startDate.setOnClickListener(v -> {
+            // TODO Auto-generated method stub
+            new DatePickerDialog(FilterGalleryActivity.this, date, myCalendar
+                    .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                    myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            dateLabel = 1;
         });
 
-        endDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                new DatePickerDialog(FilterGalleryActivity.this, date, myCalendar
-                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
-                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
-                dateLabel = 2;
-            }
+        endDate.setOnClickListener(v -> {
+            // TODO Auto-generated method stub
+            new DatePickerDialog(FilterGalleryActivity.this, date, myCalendar
+                    .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                    myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            dateLabel = 2;
         });
+
+        int MAX_LAT = 90;
+        int MIN_LAT = -90;
+        tlLat.setFilters(new InputFilter[]{ new InputFilterMinMax(MIN_LAT, MAX_LAT)});
+        brLat.setFilters(new InputFilter[]{ new InputFilterMinMax(MIN_LAT, MAX_LAT)});
+        int MAX_LON = 180;
+        int MIN_LON = -180;
+        tlLon.setFilters(new InputFilter[]{ new InputFilterMinMax(MIN_LON, MAX_LON)});
+        brLon.setFilters(new InputFilter[]{ new InputFilterMinMax(MIN_LON, MAX_LON)});
     }
 
     private void updateLabel(int label) {
         String myFormat = "MMM d, yyyy";
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
-        if (label == 1){
+        if (label == 1) {
             startDate.setText(sdf.format(myCalendar.getTime()));
-        } else if (label == 2){
+            sDate = myCalendar.getTime();
+        } else if (label == 2) {
             endDate.setText(sdf.format(myCalendar.getTime()));
+            eDate = myCalendar.getTime();
+            if ((startDate.getText() != null) && (sDate.getTime() > eDate.getTime())) {
+                Toast.makeText(FilterGalleryActivity.this, "The end date should not be earlier than the start date",
+                        Toast.LENGTH_SHORT).show();
+            }
         }
     }
 
@@ -82,16 +101,10 @@ public class FilterGalleryActivity extends AppCompatActivity {
 
     public void onFilter(final View v) {
         Intent i = new Intent();
-        EditText from = findViewById(R.id.filter_start_date);
-        EditText to = findViewById(R.id.filter_end_date);
-        EditText tlLat = findViewById(R.id.tlLat);
-        EditText tlLon = findViewById(R.id.tlLon);
-        EditText brLat = findViewById(R.id.brLat);
-        EditText brLon = findViewById(R.id.brLon);
         EditText keywords = findViewById(R.id.filter_keyword);
 
-        i.putExtra("STARTTIMESTAMP", from.getText() != null ? from.getText().toString() : "");
-        i.putExtra("ENDTIMESTAMP", to.getText() != null ? to.getText().toString() : "");
+        i.putExtra("STARTTIMESTAMP", startDate.getText() != null ? startDate.getText().toString() : "");
+        i.putExtra("ENDTIMESTAMP", endDate.getText() != null ? endDate.getText().toString() : "");
         i.putExtra("TOPLEFTLATITUDE", tlLat.getText() != null ? tlLat.getText().toString() : "");
         i.putExtra("TOPLEFTLONGITUDE", tlLon.getText() != null ? tlLon.getText().toString() : "");
         i.putExtra("BTMRIGHTLATITUDE", brLat.getText() != null ? brLat.getText().toString() : "");
