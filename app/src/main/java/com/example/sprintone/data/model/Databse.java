2f7f8224d67f81c392c6c7074b7a4d98ed8d133b
Jeffrey Stephen <jeffstep890@gmail.com>
2020-10-21 21:07:44 -0700

    supposed database model

---
+++ app/src/main/java/com/example/sprintone/data/model/Databse.java
@@ -0,0 +1,15 @@
+package com.example.sprintone.data.model;
+
+
+public class Databse{
+
+
+
+    public void getPic(Picture pic);
+    public void DeletePic(Picture pic);
+    public void SaveCaption(Picture pic);
+
+
+
+
+}
