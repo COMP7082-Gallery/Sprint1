22e0f379bd6796467435c0d2afb9d454ccc36992
Jeffrey Stephen <jeffstep890@gmail.com>
2020-10-21 22:29:14 -0700

    updated database file

--- app/src/main/java/com/example/sprintone/data/model/Databse.java
+++ app/src/main/java/com/example/sprintone/data/model/Databse.java
@@ -5,9 +5,28 @@ public class Databse{
 
 
 
-    public void getPic(Picture pic);
+    public int void getPic(Picture pic);
     public void DeletePic(Picture pic);
     public void SaveCaption(Picture pic);
+
+
+    void int getPic(Picture pic) {
+
+        //returns picture id
+
+    }
+
+    void DeletePic(Picture pic) {
+        //uses picture id to delete picture and all data related to the picture
+
+    }
+
+    public void SaveCaption(Picture pic) {
+        //uses picture id to edit caption and update.
+
+
+
+    }
 
 
 
